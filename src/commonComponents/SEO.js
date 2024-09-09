cde094200e869ac861a5763714c94ffac60c576f
Abdulqudoos-dot <wirelesstechfsd@gmail.com>
2024-09-09 16:36:43 +0500

    add tags property

--- src/commonComponents/SEO.js
+++ src/commonComponents/SEO.js
@@ -9,15 +9,11 @@ export default function SEO({
   createdAt,
   authorName,
 }) {
-  const siteUrl = "https://jewasity.com";
-  const defaultImage =
-    "https://jewasity-prod.s3.amazonaws.com/668db575-34fe-494f-b8f8-b335ec012e0c.png";
-
+  console.log("🚀 ~ title:", title);
   return (
     <Helmet>
       {/* Primary Meta Tags */}
-      <title>{title || "Jewasity"}</title>
-      <meta name="title" content={title || "Jewasity"} />
+      <meta name="title" content={ "jawwwwww"} />
       <meta
         name="description"
         content={
@@ -28,8 +24,11 @@ export default function SEO({
 
       {/* Open Graph / Facebook / WhatsApp / Telegram */}
       <meta property="og:type" content="article" />
-      <meta property="og:url" content={`${siteUrl}/blog/${redirectLink}`} />
-      <meta property="og:title" content={title || "Jewasity"} />
+      <meta
+        property="og:url"
+        content={`https://jewasity.com/blog/${redirectLink}`}
+      />
+      <meta property="og:title" content={title} />
       <meta
         property="og:description"
         content={
@@ -37,31 +36,62 @@ export default function SEO({
           "From heartwarming stories and recent music releases to current events and breaking news."
         }
       />
-      <meta property="og:image" content={image || defaultImage} />
-      <meta property="og:image:secure_url" content={image || defaultImage} />
+      <meta
+        property="og:image"
+        content={
+          image ||
+          "https://jewasity-prod.s3.amazonaws.com/668db575-34fe-494f-b8f8-b335ec012e0c.png"
+        }
+      />
+      <meta
+        property="og:image:secure_url"
+        content={
+          image ||
+          "https://jewasity-prod.s3.amazonaws.com/668db575-34fe-494f-b8f8-b335ec012e0c.png"
+        }
+      />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
-      <meta property="og:image:alt" content="Jewasity image" />
+      <meta property="og:image:alt" content="Jewasity-alt" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:site_name" content="Jewasity" />
       <meta property="og:locale" content="en_US" />
 
-      {/* Article Meta Tags */}
+      {/* Article Meta Tags for LinkedIn */}
       <meta property="article:published_time" content={createdAt} />
-      <meta property="article:author" content={authorName || "Jewasity Staff"} />
+      <meta
+        property="article:author"
+        content={authorName || "Jewasity Staff"}
+      />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={`${siteUrl}/blog/${redirectLink}`} />
-      <meta name="twitter:title" content={title || "Jewasity"} />
       <meta
+        property="twitter:url"
+        content={`https://jewasity.com/blog/${redirectLink}`}
+      />
+      <meta
+        property="blog-title"
+        name="twitter:title"
+        content={title || "Jewasity"}
+      />
+      <meta
+        property="blog-description"
         name="twitter:description"
         content={
           description ||
           "From heartwarming stories and recent music releases to current events and breaking news."
         }
       />
-      <meta name="twitter:image" content={image || defaultImage} />
+      <meta
+        name="twitter:image"
+        property="blog-image"
+        content={
+          image ||
+          "https://jewasity-prod.s3.amazonaws.com/668db575-34fe-494f-b8f8-b335ec012e0c.png"
+        }
+      />
+      <title>{title || "Jewasity"}</title>
     </Helmet>
   );
 }

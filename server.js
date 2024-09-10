bc17ef9a2898e179caca62bf2a9db5ece1201804
Abdulqudoos-dot <wirelesstechfsd@gmail.com>
2024-09-10 13:07:00 +0500

    add tags property

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
